c3a0c1de48bdaf12e151109ce3c61ff2fd7eb139
ahmed12v <ahnurmed16@gmail.com>
2026-01-11 18:14:14 +0200

    navbar finsh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,37 @@
-import { Home } from './features/home/home';
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path:'' , redirectTo:'home' , pathMatch:'full' } ,
 
-    {
-      path:'home', 
-      loadComponent:()=>
-        import('./features/home/home').then((c)=> c.Home)
-    }
+          {
+            path:'home', 
+            loadComponent:()=>
+              import('./features/home/home').then((c)=> c.Home)
+          },
+          {
+            path:'skills', 
+            loadComponent:()=>
+              import('./features/components/skills/skills').then((c)=> c.Skills)
+          },
+          {
+            path:'experience', 
+            loadComponent:()=>
+              import('./features/components/experience/experience').then((c)=> c.Experience)
+          },
+          {
+            path:'projects', 
+            loadComponent:()=>
+              import('./features/components/projects/projects').then((c)=> c.Projects)
+          },
+          {
+            path:'courses', 
+            loadComponent:()=>
+              import('./features/components/courses/courses').then((c)=> c.Courses)
+          },
+          {
+            path:'contact', 
+            loadComponent:()=>
+              import('./features/components/contact/contact').then((c)=> c.Contact)
+          }
 ];
--- src/app/layouts/nav/nav.ts
+++ src/app/layouts/nav/nav.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 
 }
