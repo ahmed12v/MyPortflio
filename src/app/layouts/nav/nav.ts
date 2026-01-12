b69ac7af609d54ff016594289cf789e78f4fb648
ahmed12v <ahnurmed16@gmail.com>
2026-01-12 16:34:02 +0200

    nav-scroll

--- src/app/layouts/nav/nav.ts
+++ src/app/layouts/nav/nav.ts
@@ -16,16 +16,19 @@ export class Nav {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+    document.body.classList.add('no-scroll');
+  } else {
+    document.body.classList.remove('no-scroll');
+  }
   }
   closeMenu() {
     this.menuOpen = false;
+    document.body.classList.remove('no-scroll');
   }
+
   openMycv(){
     window.open("https://drive.google.com/file/d/1zXvT6AtexNgjAvy4XzoptfmecV7cpr9B/view?usp=drive_link", "_blank");
   }
-  
-
-
-
 
 }
